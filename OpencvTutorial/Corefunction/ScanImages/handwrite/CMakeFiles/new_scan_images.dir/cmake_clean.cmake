eabb24efaa7d6633527029d1da06df4c53d29a51
truyenspidermanvn <a@vnn.vn>
2017-10-22 21:05:01 +0700

    handwrite new

---
+++ OpencvTutorial/Corefunction/ScanImages/handwrite/CMakeFiles/new_scan_images.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/new_scan_images.dir/new_scan_images.cpp.o"
+  "new_scan_images.pdb"
+  "new_scan_images"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/new_scan_images.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
